5af6e705e303588d13cafe4f5bf2f317a415901d
Afshin Bozorgpour <afshinbigboy@gmail.com>
2023-02-05 03:55:59 +0330

    added isic2018

--- scripts/segmentation_train.py
+++ scripts/segmentation_train.py
@@ -6,7 +6,7 @@ sys.path.append(".")
 from guided_diffusion import dist_util, logger
 from guided_diffusion.resample import create_named_schedule_sampler
 from guided_diffusion.bratsloader import BRATSDataset
-from guided_diffusion.isicloader import ISICDataset
+from guided_diffusion.isicloader import ISICDataset, ISIC2018Dataset
 from guided_diffusion.script_util import (
     model_and_diffusion_defaults,
     create_model_and_diffusion,
@@ -33,16 +33,29 @@ def main():
 
         ds = ISICDataset(args, args.data_dir, transform_train)
         args.in_ch = 4
+    if args.data_name == 'ISIC2018':
+        tran_list = [transforms.Resize((args.image_size,args.image_size)), transforms.ToTensor(),]
+        transform_train = transforms.Compose(tran_list)
+
+        ds = ISIC2018Dataset(args, args.data_dir, transform_train)
+        args.in_ch = 4
     elif args.data_name == 'BRATS':
         tran_list = [transforms.Resize((args.image_size,args.image_size)),]
         transform_train = transforms.Compose(tran_list)
 
         ds = BRATSDataset(args.data_dir, transform_train, test_flag=False)
         args.in_ch = 5
-    datal= th.utils.data.DataLoader(
+    
+    datal = th.utils.data.DataLoader(
         ds,
         batch_size=args.batch_size,
         shuffle=True)
+    if args.data_name == 'ISIC2018':
+        datal = th.utils.data.Subset(datal, range(0       , 1815  +250  ))
+        # vl_dataset = th.utils.data.Subset(dataset, range(1815    , 1815+259    ))
+        # te_dataset = th.utils.data.Subset(dataset, range(1815+259, 1815+259+520))
+    
+    
     data = iter(datal)
 
     logger.log("creating model and diffusion...")
